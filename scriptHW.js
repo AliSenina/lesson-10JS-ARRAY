162145ba963a22ef372d826ef6cedecec0cc9140
AliSenina <122289718+AliSenina@users.noreply.github.com>
2023-03-09 18:49:26 +0200

    Add files via upload

---
+++ scriptHW.js
@@ -0,0 +1,47 @@
+
+// Task1
+
+// function arrCopy(arr) {
+//     let newArr = [];
+
+//        newArr = arr.concat([...newArr]);
+
+
+//     return newArr;
+// }
+
+
+// const arr1 = console.log(arrCopy([1, 2, 3]))
+// const arr2 = console.log(arrCopy([1, 2, 3,[4,5]]))
+
+// TAsk2
+
+// const arrToString=(arr)=>console.log(arr.toString());
+
+
+// const arr1 = arrToString([1, 2, 3])
+// const arr2 = arrToString([10, 200, 3333])
+
+// Task3
+
+// let getLength = (arr) => arr.forEach(element => {
+//     let newArr = new Array();
+//     newArr = element.length
+//     console.log(newArr)
+
+// });
+// const arr1 = getLength(['Ivan', 'Pavlo', 'Ira'])
+// const arr2 = getLength(['Oleksiy','Andriana'])
+
+// Task4
+// function removeDuplicates(arr){
+//     return arr.filter((item,
+//         index) => arr.indexOf(item) === index);
+
+// }
+
+
+// const arr1 = removeDuplicates(['html', 'css', 'html', 'js'])
+// console.log(arr1)
+// const arr2 = removeDuplicates(['html', 'css', 'js', 'html', 'js', 'python', 'js', 'scss'])
+// console.log(arr2)
